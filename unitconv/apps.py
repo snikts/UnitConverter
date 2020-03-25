8473ab26f69e31fbb57f0332861cbc0608754136
Sarah Jones <sarahjones@Sarahs-MacBook-Pro-2.local>
2020-03-25 13:13:41 -0600

    Debugging Commit

---
+++ unitconv/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UnitconvConfig(AppConfig):
+    name = 'unitconv'
---
+++ unitconv/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse
+import datetime
+# Create your views here.
+
+
+def convert(request):
+
+    if 'from' not in request.GET:
+        resp = {'error': "Usage: from=[T, g, t_oz, kg, lb, or oz]"}
+    else:
+        f = request.GET['from']
+        v = request.GET['value']
+        if f != 'T' and f!= 'g' and f!= 't_oz' and f!='kg' and f!='lb' and f!='oz':
+            resp = {'error': "Usage: from=[T, g, t_oz, kg, lb, or oz]"}
+        else:
+            data = request.GET['myData']
+            if f == 'T':
+                conversionVal = 29166.7
+                amount = conversionVal*data
+                resp = {{'units': request.GET['to']}, {'value': amount}}
+            elif f == 'g':
+                conversionVal = 0.0322
+                amount = conversionVal*data
+                resp = {{'units': request.GET['to']}, {'value': amount}}
+            elif f == 't_oz':
+                conversionVal = 1
+                amount = conversionVal*data
+                resp = {{'units': request.GET['to']}, {'value': amount}}
+            elif f == 'kg':
+                conversionVal = 32.15
+                amount = conversionVal*data
+                resp = {{'units': request.GET['to']}, {'value': amount}}
+            elif f == 'lb':
+                conversionVal = 14.58
+                amount = conversionVal*data
+                resp = {{'units': request.GET['to']}, {'value': amount}}
+            elif f == 'oz':
+                conversionVal = 0.912
+                amount = conversionVal *data
+                resp = {{'units': request.GET['to']}, {'value': amount}}
+    # return JsonResponse(resp)
+    return render(request, 'index.html', {'returnVal': resp})
+
+
